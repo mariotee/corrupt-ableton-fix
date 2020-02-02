fd7047add47d154f7a8f8d5f9630f7a68df309cd
mariotee <mariotorresdev@gmail.com>
2020-02-02 13:11:19 -0600

    added more accurate parsing for arrays

--- index.js
+++ index.js
@@ -8,31 +8,53 @@ function keyEndsInId(name) {
     return name.match(/id$/i)
 }
 
+function noInnerObjects(obj) {
+    for (const key of Object.keys(obj)) {
+        if (obj[key] instanceof Object) {
+            return false
+        }
+    }
+
+    return true
+}
+
 function findDuplicates(node, parentPath, level) {
     const isObj = (element) => element instanceof Object && !(element instanceof Array)
     const isArr = (element) => element instanceof Object && (element instanceof Array)
 
-    if (isObj(node)) {                
-        const vals = []
+    if (isObj(node)) {
+        const vals = []        
         for (const key of Object.keys(node)) {
             const path = parentPath + " > " + key
 
-            if (vals.includes(node[key]) && keyEndsInId(key)) {                               
+            if (vals.includes(node[key]) && keyEndsInId(key)) {
                 fs.appendFileSync(filename,`DUPE PATH = (${path}) ; VALUE = ${node[key]}`)
-                fs.appendFileSync(filename,"\n")                
-            }            
+                fs.appendFileSync(filename,"\n")
+            }
 
             vals.push(node[key])
 
             findDuplicates(node[key], path, level+1)
         }
     } else if (isArr(node)) {
-        for (const index in node) {
-            findDuplicates(node[index], `${parentPath}[${index}]`, level+1)
+        const vals = []
+        for (const index in node) {            
+            let thing = node[index]
+            if (noInnerObjects(thing)) {                               
+                for (const key of Object.keys(thing)) {
+                    const path = parentPath + " > " + key
+
+                    if (vals.includes(thing[key]) && keyEndsInId(key)) {
+                        fs.appendFileSync(filename,`DUPE PATH = (${path}) ; VALUE = ${thing[key]}`)
+                        fs.appendFileSync(filename,"\n")
+                    }
+
+                    vals.push(thing[key])
+                }
+            } else {
+                findDuplicates(node[index], `${parentPath}[${index}]`, level+1)
+            }
         }
-    } else {
-        //DEBUGGING
-        //parentPath.match(/_noteid$/i) && console.log(parentPath)
     }
 }
 
